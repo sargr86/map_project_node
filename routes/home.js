723bab7a4d324346daadf6ee709a8b73ad5db8a0
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-15 18:00:55 +0400

    Home page fixes

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,8 @@
 const router = express.Router();
 const homeController = require('../controllers/homeController');
+const ferriesController = require('../controllers/ferriesController');
 
-router.get('/get_places', homeController.get);
+router.get('/get_places', ferriesController.get);
 
 
 module.exports = router;
