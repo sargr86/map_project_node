e1b4131d5b4ad74995fc3028eadb4f3bb60707b3
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-15 18:00:55 +0400

    Home page fixes

--- validators/validatePartner.js
+++ validators/validatePartner.js
@@ -4,7 +4,7 @@ const rules = [
     body('first_name').not().isEmpty().withMessage('First name is required'),
     body('email').not().isEmpty().withMessage('E-mail is required').isEmail().withMessage('Email is invalid'),
     body('last_name').not().isEmpty().withMessage('Last name is required'),
-    body('password').not().isEmpty().withMessage('Password is required'),
+    body('pass').not().isEmpty().withMessage('Password is required'),
     body('type').not().isEmpty().withMessage('Partner type is required'),
     body().custom(async (req) => {
         let lang = req.lang;
