4f0fee21fb4811fa40b431add823f3107b27b9b0
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-15 16:38:14 +0400

    Removed timestamps columns from ferries migration

--- migrations/20190409092556-create-ferries.js
+++ migrations/20190409092556-create-ferries.js
@@ -37,14 +37,6 @@ module.exports = {
       },
       type: {
         type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
