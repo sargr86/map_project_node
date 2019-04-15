04c8d64dad4236ae08e1aac69b11a416a5033eab
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-15 14:24:42 +0400

    Fixed url for the middleware

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.get('*', (req,res)=>{
 //res.sendFile(path.join(__dirname,`../../secret_south/secret_south_angular/dist/front/${req.url}`))
         res.sendFile(url);
     } else {
-        console.log(url)
         res.sendFile(path.join(__dirname,'../../secret_south/secret_south_angular/dist/front/index.html'));
     }
     //res.sendFile(path.join(__dirname,'../../secret_south/secret_south_angular/dist/front/index.html'))
