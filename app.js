a340ab0e206af761c0c62aa22b443e4f023d6a5a
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-17 18:16:06 +0400

    Permissions test

--- app.js
+++ app.js
@@ -22,33 +22,33 @@ app.use('/home', require('./routes/home'));
 
 
 // Admin middleware
-// app.use((req, res, next) => {
-//     let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-//     if(!token) {
-//         res.status(500).json('Auth token is not supplied');
-//     }
-//     if (token.startsWith('Bearer ')) {
-//         // Remove Bearer from string
-//         token = token.slice(7, token.length);
-//     }
-//
-//     if (token) {
-//         jwt.verify(token, 'secretkey', (err, decoded) => {
-//             if (err) {
-//                 return res.json({
-//                     success: false,
-//                     message: 'Token is not valid'
-//                 });
-//             } else {
-//                 req.decoded = decoded;
-//                 next();
-//             }
-//         });
-//     } else {
-//         return res.status(500).json('Auth token is not supplied');
-//     }
-//     next();
-// });
+app.use((req, res, next) => {
+    let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+    if(!token) {
+        res.status(500).json('Auth token is not supplied');
+    }
+    if (token.startsWith('Bearer ')) {
+        // Remove Bearer from string
+        token = token.slice(7, token.length);
+    }
+
+    if (token) {
+        jwt.verify(token, 'secretkey', (err, decoded) => {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Token is not valid'
+                });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(500).json('Auth token is not supplied');
+    }
+    // next();
+});
 
 // Auth Routes
 app.use('/users', require('./routes/users'));
