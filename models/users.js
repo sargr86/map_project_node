a3a52400bff40c751577e97e0bab420f467711fb
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-18 15:24:12 +0400

    ----- BREAKING CHANGE ------ Moved partners to users table

--- models/users.js
+++ models/users.js
@@ -1,22 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     const users = sequelize.define('users', {
+        role_id: DataTypes.INTEGER,
         status_id: DataTypes.INTEGER,
-        first_name_en: DataTypes.STRING,
-        last_name_en: DataTypes.STRING,
-        first_name_ru: DataTypes.STRING,
-        last_name_ru: DataTypes.STRING,
-        first_name_hy: DataTypes.STRING,
-        last_name_hy: DataTypes.STRING,
+        partner_type_id: DataTypes.INTEGER,
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
         birthday: DataTypes.DATEONLY,
         gender: DataTypes.STRING,
         email: DataTypes.STRING,
         password: DataTypes.STRING,
-        profile_img: DataTypes.STRING
+        profile_img: DataTypes.STRING,
     }, {underscored: true});
     users.associate = function (models) {
-        users.belongsToMany(models.roles, {through: models.users_roles, foreignKey: 'user_id'})
+        // users.belongsToMany(models.roles, {through: models.users_roles, foreignKey: 'user_id'})
         users.belongsTo(models.users_statuses, {foreignKey: 'status_id'})
+        users.belongsTo(models.roles, {foreignKey: 'role_id'})
     };
     return users;
 };
