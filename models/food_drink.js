6a6cefbe70a799280a05d527f4ad3b54f7d6e95e
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-22 16:57:34 +0400

    Added food-drink crud

--- models/food_drink.js
+++ models/food_drink.js
@@ -1,16 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const food_drink = sequelize.define('food_drink', {
-    type_id: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    lat: DataTypes.FLOAT,
-    lng: DataTypes.FLOAT,
-    address: DataTypes.TEXT,
-    img: DataTypes.STRING
-  }, {});
-  food_drink.associate = function(models) {
-    // associations can be defined here
-  };
-  return food_drink;
+    const food_drink = sequelize.define('food_drink', {
+        type_id: DataTypes.INTEGER,
+        partner_id: DataTypes.INTEGER,
+        name: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        lat: DataTypes.FLOAT,
+        lng: DataTypes.FLOAT,
+        address: DataTypes.TEXT,
+        img: DataTypes.STRING
+    }, {timestamps: false, underscore: true});
+    food_drink.associate = function (models) {
+        // associations can be defined here
+        food_drink.belongsTo(models.users, {foreignKey: 'partner_id'})
+    };
+    return food_drink;
 };
--- routes/food_drink.js
+++ routes/food_drink.js
@@ -1,11 +1,12 @@
 const router = express.Router();
 const foodDrinkController = require('../controllers/foodDrinkController');
-const validateTourType = require('../validators/validateTourType');
+const validateFoodDrink = require('../validators/validateFoodDrink');
 
 router.get('/get', foodDrinkController.get);
-// router.get('/getOne', toursController.getOneTourType);
-// router.post('/add',validateTourType.rules,toursController.addTourType);
-// router.put('/update', validateTourType.rules, toursController.updateTourType);
-// router.delete('/remove', toursController.removeTourType);
+router.get('/get-partners', foodDrinkController.getPartners);
+router.get('/getOne', foodDrinkController.getOne);
+router.post('/add',validateFoodDrink.rules,foodDrinkController.add);
+router.put('/update', validateFoodDrink.rules, foodDrinkController.update);
+router.delete('/remove', foodDrinkController.remove);
 
 module.exports = router;
