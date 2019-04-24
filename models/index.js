2a1bd5552e30c57dcc798c4ce4849d63571295ef
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-24 11:32:35 +0400

    Handled sequelize authentication/connection error

--- models/index.js
+++ models/index.js
@@ -10,25 +10,42 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+
+// Handling sequelize db connecting for all requests
+app.get('*', (req, res, next) => {
+
+    sequelize.authenticate()
+        .then(() => {
+            next();
+        })
+        .catch(err => {
+            res.status(422).json({db_error: err.toString()});
+        });
+    if (!res.headersSent) {
+        next();
+    }
+});
+
+
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
