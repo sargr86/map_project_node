58e2eac67ad25cf4f228f96c06a3bef91602318d
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-26 15:05:32 +0400

    Tested the rest forms validation

--- validators/validateFoodDrink.js
+++ validators/validateFoodDrink.js
@@ -1,28 +1,17 @@
 require('../constants/sequelize');
 
 const rules = [
-    body('name', 'Name is required').not().isEmpty(),
-    body('lat', 'Latitude is required').not().isEmpty(),
-    body('lng', 'Longitude is required').not().isEmpty(),
-    body().custom(async (req) => {
-
-        if (!validatePattern(req.lat, 'lat')) {
-            throw new Error('Latitude is invalid')
-        } else if (!validatePattern(req.lng, 'lng')) {
-            throw new Error('Longitude is invalid')
-        }
-
-
-        // Retrieving a user with request email
-        let tourName = await FoodDrink.findOne({where: {name: req.name}});
-
-        if (tourName != null && !req.id) throw new Error('Name exists');
-
-        else return true;
-    }),
+    body('name', 'Name is required').not().isEmpty()
+    // Retrieving a tour with request name and checking tour existence
+        .custom(async (name, {req}) => {
+            let partner = await FoodDrink.findOne({where: {name: name}, attributes: ['name']});
+            return !((partner != null && !req.body.id));
+        }).withMessage('Food/drink name exists'),
+    body('description', 'Description is required').not().isEmpty(),
+    body('lat').not().isEmpty().withMessage('Latitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Latitude is invalid'),
+    body('lng').not().isEmpty().withMessage('Longitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Longitude is invalid'),
     body('address', 'Address is required').not().isEmpty(),
     body('partner_id', 'Partner is required').not().isEmpty()
-
 
 ];
 
--- validators/validateTour.js
+++ validators/validateTour.js
@@ -1,32 +1,18 @@
 require('../constants/sequelize');
 
 const rules = [
-    body('name', 'Tour name is required').not().isEmpty(),
-    body('lat', 'Tour latitude is required').not().isEmpty(),
-    body('lng', 'Tour longitude is required').not().isEmpty(),
-    body().custom(async (req) => {
-
-        if(!validatePattern(req.lat,'lat')){
-            throw new Error('Tour latitude is invalid')
-        }
-
-        else if(!validatePattern(req.lng,'lng')){
-            throw new Error('Tour longitude is invalid')
-        }
-
-
-
-        // Retrieving a user with request email
-        let tourName = await Tours.findOne({where: {name: req.name}});
-
-        if (tourName != null && !req.id) throw new Error('Tour name exists');
-
-        else return true;
-    }),
+    body('name', 'Tour name is required').not().isEmpty()
+        // Retrieving a tour with request name and checking tour existence
+        .custom(async (name, {req}) => {
+            let partner = await Tours.findOne({where: {name: name}, attributes: ['name']});
+            return !((partner != null && !req.body.id));
+        }).withMessage('Tour name exists'),
+    body('lat').not().isEmpty().withMessage('Tour latitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Tour latitude is invalid'),
+    body('lng').not().isEmpty().withMessage('Tour longitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Tour longitude is invalid'),
     body('address', 'Tour address is required').not().isEmpty(),
-    body('tours_type_id','Tour type is required').not().isEmpty(),
-    body('partner_id','Tour partner is required').not().isEmpty()
-
+    body('tours_type_id', 'Tour type is required').not().isEmpty(),
+    body('partner_id', 'Tour partner is required').not().isEmpty(),
+    body('img').custom((img, {req}) => req.body.id || img).withMessage('Tour image is required'),
 
 ];
 
