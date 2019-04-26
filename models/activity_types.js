fd6ef1eaa387a59c63b56e629de6e0bff40752fa
Sofia Bruno <sofiabruno19@yahoo.com>
2019-04-26 13:58:04 +0400

    Checked accommodation/activity types/ferries sections validation

--- models/activity_types.js
+++ models/activity_types.js
@@ -1,11 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const activity_types = sequelize.define('activity_types', {
-    name: DataTypes.STRING
-  }, {timestamps:false,underscored:true});
-  activity_types.associate = function(models) {
-    // associations can be defined here
-    activity_types.belongsTo(models.activities, {foreignKey: "id"});
-  };
-  return activity_types;
+    const activity_types = sequelize.define('activity_types', {
+        name: DataTypes.STRING,
+        description: DataTypes.TEXT
+    }, {timestamps: false, underscored: true});
+    activity_types.associate = function (models) {
+        // associations can be defined here
+        activity_types.belongsTo(models.activities, {foreignKey: "id"});
+    };
+    return activity_types;
 };
--- validators/validateFerry.js
+++ validators/validateFerry.js
@@ -1,32 +1,20 @@
 require('../constants/sequelize');
 
 const rules = [
-    body('name').not().isEmpty().withMessage('Name is required'),
-    // body('email').not().isEmpty().withMessage('E-mail is required').isEmail().withMessage('Email is invalid'),
-    body('max_people').not().isEmpty().withMessage('Max people is required'),
+    body('name').not().isEmpty().withMessage('Name is required')
+        .custom(async(name, {req}) => {
+            // Retrieving a ferry with request name
+            let partner = await Ferries.findOne({where: {name: name}, attributes: ['name']});
+            return !( (partner != null && !req.id) );
+        }).withMessage('Ferry name exists'),
+    body('max_people').not().isEmpty().withMessage('Max people is required')
+        .custom((max, {req}) => max > req.body.min_people).withMessage('Max people must be greater than Min people count'),
     body('min_people').not().isEmpty().withMessage('Min people is required'),
-    body('lat').not().isEmpty().withMessage('Latitude is required'),
-    body('lng').not().isEmpty().withMessage('Longitude is required'),
+    body('lat').not().isEmpty().withMessage('Latitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Latitude is invalid'),
+    body('lng').not().isEmpty().withMessage('Longitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Longitude is invalid'),
     body('phone').not().isEmpty().withMessage('Phone number is required'),
     body('address').not().isEmpty().withMessage('Address is required'),
-    // body('type').not().isEmpty().withMessage('Partner type is required'),
     body('partner_id').not().isEmpty().withMessage('Partner is required'),
-    body().custom(async (req) => {
-        let email = req.email;
-
-        if (req.min_people >= req.max_people) {
-            throw new Error('Max people must be greater than Min people count');
-        }
-
-        // Retrieving a ferry with request email
-        let partner = await Ferries.findOne({where: {email: email}, attributes: ['email']});
-
-        // Checking if ferry email exists
-        if (partner != null && !req.id) throw new Error('E-mail exists');
-
-    }),
-
-
 ];
 
 module.exports = {
