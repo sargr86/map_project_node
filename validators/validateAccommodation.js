5257348e57ca5df1248e4275e335fb83de111c86
Sofia Bruno <sofiabruno3003@gmail.com>
2019-04-26 13:58:04 +0400

    Checked accommodation/activity types/ferries sections validation

--- validators/validateAccommodation.js
+++ validators/validateAccommodation.js
@@ -1,25 +1,14 @@
 require('../constants/sequelize');
 
 const rules = [
-    body('name', 'Name is required').not().isEmpty(),
-    body('lat', 'Latitude is required').not().isEmpty(),
-    body('lng', 'Longitude is required').not().isEmpty(),
-    body().custom(async (req) => {
+    body('name').not().isEmpty().withMessage('Accommodation name is required').custom(async (name,{req}) => {
+        // Retrieving an accommodation with request name
+        let tourName = await Accommodations.findOne({where: {name: name}});
+        return !(tourName != null && !req.body.id);
 
-        if (!validatePattern(req.lat, 'lat')) {
-            throw new Error('Latitude is invalid')
-        } else if (!validatePattern(req.lng, 'lng')) {
-            throw new Error('Longitude is invalid')
-        }
-
-
-        // Retrieving a user with request email
-        let tourName = await Accommodations.findOne({where: {name: req.name}});
-
-        if (tourName != null && !req.id) throw new Error('Name exists');
-
-        else return true;
-    }),
+    }).withMessage('Accommodation name exists'),
+    body('lat').not().isEmpty().withMessage('Latitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Latitude is invalid'),
+    body('lng').not().isEmpty().withMessage('Longitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Longitude is invalid'),
     body('address', 'Address is required').not().isEmpty(),
     body('partner_id', 'Partner is required').not().isEmpty()
 
