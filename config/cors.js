1a4234d24e16143d15a86a2ec659802d21889106
Sofia Bruno <sofiabruno3003@gmail.com>
2019-05-14 18:13:55 +0400

    Cors whitelisted domains update

--- config/cors.js
+++ config/cors.js
@@ -3,6 +3,7 @@ let originsWhitelist = [
     'http://localhost:4202',
     'http://localhost:4201',     //this is my front-end url for development,
     'http://68.183.36.96:80',
+    'http://68.183.36.96',
     'http://secretsouth.ie/'
 ];
 let corsOptions = {
