b7f0c6441ca13276f4f700f7b8a0411155a22bc3
Sofia Bruno <sofiabruno3003@gmail.com>
2019-05-23 14:35:05 +0400

    Improvements for invitations/register process

--- routes/employees.js
+++ routes/employees.js
@@ -2,14 +2,15 @@ const router = express.Router();
 const employeesController = require('../controllers/employeesController');
 const validatePartner = require('../validators/validatePartner');
 const validateLogin = require('../validators/validateLogin');
+const validateInvite = require('../validators/validateInvite');
 
 router.post('/login', validateLogin.rules, employeesController.login);
 router.get('/get', employeesController.get);
 router.get('/getTypes', employeesController.getTypes);
-router.get('/getOne',checkAdmin, employeesController.getOne);
-router.post('/add',checkAdmin, uploadProfileImg,validatePartner.rules, employeesController.add);
-router.put('/update',checkAdmin, validatePartner.rules, employeesController.update);
-router.delete('/remove',checkAdmin, employeesController.remove);
-router.post('/invite', employeesController.invite)
+router.get('/getOne', checkAdmin, employeesController.getOne);
+router.post('/add', checkAdmin, uploadProfileImg, validatePartner.rules, employeesController.add);
+router.put('/update', checkAdmin, validatePartner.rules, employeesController.update);
+router.delete('/remove', checkAdmin, employeesController.remove);
+router.post('/invite', validateInvite.rules, employeesController.invite)
 
 module.exports = router;
