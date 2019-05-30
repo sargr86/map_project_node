3a250b1e032df1e850c7af645237d34a208902a6
Sofia Bruno <sofiabruno3003@gmail.com>
2019-05-30 12:46:18 +0400

    Changed seeders and migrations for companies and users

--- models/users.js
+++ models/users.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
     const users = sequelize.define('users', {
         role_id: DataTypes.INTEGER,
         status_id: DataTypes.INTEGER,
+        company_id: DataTypes.INTEGER,
         partner_type_id: DataTypes.INTEGER,
         first_name: DataTypes.STRING,
         last_name: DataTypes.STRING,
