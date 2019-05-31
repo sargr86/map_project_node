b28b07bbcde3cb3c345dde6094d3011e68f3d60f
Conrad Joyner <conradjoyner@yahoo.com>
2019-05-31 17:21:24 +0400

    Angular routes fixes on server

--- routes/tours.js
+++ routes/tours.js
@@ -5,11 +5,9 @@ const validateTour = require('../validators/validateTour');
 router.get('/get', toursController.get);
 router.get('/get-partners', toursController.getPartners);
 
-router.use(checkAuth);
-
-router.get('/getOne', checkRole('admin','partner'), toursController.getOne);
-router.post('/add', checkRole('admin','partner'), uploadTourImg, validateTour.rules, toursController.add);
-router.put('/update', checkRole('admin','partner'), uploadTourImg, validateTour.rules, toursController.update);
-router.delete('/remove', checkRole('admin','partner'), toursController.remove);
+router.get('/getOne', checkAuth, checkRole('admin', 'partner'), toursController.getOne);
+router.post('/add', checkAuth, checkRole('admin', 'partner'), uploadTourImg, validateTour.rules, toursController.add);
+router.put('/update', checkAuth, checkRole('admin', 'partner'), uploadTourImg, validateTour.rules, toursController.update);
+router.delete('/remove', checkAuth, checkRole('admin', 'partner'), toursController.remove);
 
 module.exports = router;
