925def4aedfef6a826a27312940f70a911b4b567
Sofia Bruno <sofiabruno3003@gmail.com>
2019-06-06 18:03:52 +0400

    Optimized accommodations form, added default tour types seeder, added build-form-data pipe

--- routes/accommodations.js
+++ routes/accommodations.js
@@ -5,8 +5,8 @@ const validateAccommodation = require('../validators/validateAccommodation');
 router.get('/get', accommodationsController.get);
 router.get('/get-partners', accommodationsController.getPartners);
 router.get('/getOne', accommodationsController.getOne);
-router.post('/add',validateAccommodation.rules,accommodationsController.add);
-router.put('/update', validateAccommodation.rules, accommodationsController.update);
+router.post('/add', uploadTourImg, validateAccommodation.rules, accommodationsController.add);
+router.put('/update', uploadTourImg, validateAccommodation.rules, accommodationsController.update);
 router.delete('/remove', accommodationsController.remove);
 
 module.exports = router;
