c1c16748a00e995f26b5665c07ba2f1c6593a35b
Sofia Bruno <finn@webmasterfreelanceonlinedev.site>
2019-06-07 16:48:29 +0400

    Added company field to ferries form

--- validators/validateFerry.js
+++ validators/validateFerry.js
@@ -4,7 +4,9 @@ const rules = [
     body('name').not().isEmpty().withMessage('Name is required')
         .custom(async (name, {req}) => {
             // Retrieving a ferry with request name
-            let partner = await Ferries.findOne({where: {name: name}, attributes: ['name']});
+            let partner = await Ferries.findOne({
+                where: {name: name}, attributes: ['name']
+            });
             return !((partner != null && !req.body.id));
         }).withMessage('Ferry name exists'),
     body('max_people').not().isEmpty().withMessage('Max people is required')
@@ -12,11 +14,10 @@ const rules = [
     body('min_people').not().isEmpty().withMessage('Min people is required'),
     body('lat').not().isEmpty().withMessage('Latitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Latitude is invalid'),
     body('lng').not().isEmpty().withMessage('Longitude is required').custom((lat) => validatePattern(lat, 'lat')).withMessage('Longitude is invalid'),
-    // body('phone').not().isEmpty().withMessage('Phone number is required'),
     body('address').not().isEmpty().withMessage('Address is required'),
-    body('partner_id').custom((partner_id, {req}) => {
-        if (req.decoded.role.name_en.toLocaleLowerCase() === 'admin' && !req.body.partner_id) {
-           throw new Error('Partner is required');
+    body('company_id').custom((company_id, {req}) => {
+        if (req.decoded.role.name_en.toLocaleLowerCase() === 'admin' && !req.body.company_id) {
+            throw new Error('Company is required');
         }
         return true;
     }),
