bb5806782f7ad41e6293a33dcff6181a9a5e61f7
Sofia Bruno <sofiabruno3003@gmail.com>
2019-07-02 18:06:53 +0400

    Added accommodation seeders fix

--- migrations/20190424093412-create-accommodations.js
+++ migrations/20190424093412-create-accommodations.js
@@ -1,40 +1,43 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('accommodations', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      company_id: {
-        type: Sequelize.INTEGER
-      },
-      partner_id: {
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.TEXT
-      },
-      lat: {
-        type: Sequelize.STRING
-      },
-      lng: {
-        type: Sequelize.STRING
-      },
-      address: {
-        type: Sequelize.TEXT
-      },
-      img: {
-        type: Sequelize.STRING
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('accommodations');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('accommodations', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            company_id: {
+                type: Sequelize.INTEGER
+            },
+            type_id: {
+                type: Sequelize.INTEGER
+            },
+            partner_id: {
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING
+            },
+            description: {
+                type: Sequelize.TEXT
+            },
+            lat: {
+                type: Sequelize.STRING
+            },
+            lng: {
+                type: Sequelize.STRING
+            },
+            address: {
+                type: Sequelize.TEXT
+            },
+            img: {
+                type: Sequelize.STRING
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('accommodations');
+    }
 };
--- models/accommodations.js
+++ models/accommodations.js
@@ -2,12 +2,13 @@
 module.exports = (sequelize, DataTypes) => {
     const accommodation = sequelize.define('accommodations', {
         company_id: DataTypes.INTEGER,
+        type_id: DataTypes.INTEGER,
         name: DataTypes.STRING,
         description: DataTypes.TEXT,
         lat: DataTypes.STRING,
         lng: DataTypes.STRING,
         address: DataTypes.TEXT,
-        img: DataTypes.STRING
+        img: DataTypes.STRING,
     }, {timestamps: false, underscored: true});
     accommodation.associate = function (models) {
         // associations can be defined here
