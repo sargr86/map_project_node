fca2887441a07d4fcc64f9179a92701a00be2a49
Sofia Bruno <sofiabruno3003@gmail.com>
2019-11-06 10:18:11 +0400

    Added path aliases, minor changes

--- validators/validateLogin.js
+++ validators/validateLogin.js
@@ -1,15 +1,15 @@
-require('../constants/sequelize')
+require('../constants/sequelize');
 
 const rules = [
     body('email').not().isEmpty().withMessage('E-mail is required').isEmail().withMessage('E-mail is invalid'),
     body('password', 'Password is required').not().isEmpty(),
-    body().custom(async(req)=>{
+    body().custom(async (req) => {
         let email = req.email;
         let pass = req.password;
 
         // Checking email existence & passwords match
-        let found = await Users.findOne({attributes:['email','password'],where:{email:email}});
-        if(!found) throw new Error('A user with such email doesn\'t exist');
+        let found = await Users.findOne({attributes: ['email', 'password'], where: {email: email}});
+        if (!found) throw new Error('A user with such email doesn\'t exist');
         let match = await bcrypt.compare(pass, found.password);
 
         // Passwords mismatch case
