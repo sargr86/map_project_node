43dcdcd6c509156b2e55555fae52e651165b5864
Sofia Bruno <ivan.nikitich.90@inbox.ru>
2020-01-10 11:16:03 +0400

    Fixed ferries cover image setting, removed duplicate code for cover setting functionality

--- constants/directories.js
+++ constants/directories.js
@@ -1,4 +1,4 @@
-global.UPLOADS_FOLDER = path.join(__dirname, '../public/uploads/');
+global.UPLOADS_FOLDER = path.join(__dirname, '../public/uploads');
 global.USERS_UPLOAD_FOLDER = path.join(UPLOADS_FOLDER, 'users/');
 global.OTHERS_UPLOAD_FOLDER = path.join(UPLOADS_FOLDER, 'others/');
 global.TOURS_UPLOAD_FOLDER = path.join(UPLOADS_FOLDER, 'others/tours');
