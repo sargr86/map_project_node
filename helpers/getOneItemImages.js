aa28c38b2df358fa814b590484acdcb9f1f1dfc8
Sofia Bruno <ivan.nikitich.90@inbox.ru>
2020-01-10 11:45:03 +0400

    Fix for ferries controller

--- helpers/getOneItemImages.js
+++ helpers/getOneItemImages.js
@@ -2,12 +2,13 @@ const {promisify} = require('util');
 const readdir = promisify(require('fs-extra').readdir);
 
 
-module.exports = async (req, uploadFolder, dbData, gallery = true) => {
+module.exports = async (req, uploadFolder, dbData) => {
     let files;
     if (dbData) {
         dbData = dbData.toJSON();
         const folder = path.resolve(uploadFolder + '/' + dbData['name'].replace(/ /g, '_'));
         const realFolder = 'http://' + req.headers.host + path.relative('./', folder).replace(/\\/g, '/').replace('public', '');
+        console.log(realFolder)
         const folderPath = path.relative('./', folder).replace(/\\/g, '/').replace('public', '');
         if (fse.existsSync(folder)) {
             // Do something
