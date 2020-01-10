19843f57c875d5108c7af1867628eddb57fc07e7
Sofia Bruno <ivan.nikitich.90@inbox.ru>
2020-01-10 11:47:14 +0400

    Fix for ferries controller

--- controllers/ferriesController.js
+++ controllers/ferriesController.js
@@ -19,7 +19,6 @@ exports.get = async (req, res) => {
     const result = await to(Ferries.findAll(wherePartner));
 
     if (!res.headersSent) {
-        console.log('in')
         res.json(result);
     }
 };
