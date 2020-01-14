ba7c3a0ca8389b52e3c6742d41b1a6681ab6252f
Sofia Bruno <ivan.nikitich.90@inbox.ru>
2020-01-14 11:59:09 +0400

    Added Google auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,29 @@ const authController = require('../controllers/authController');
 const validateRegister = require('../validators/validateRegister');
 const validateProfile = require('../validators/validateProfile');
 const validateLogin = require('../validators/validateLogin');
+const passport = require('passport');
 
 router.post('/register', uploadProfileImg, validateRegister.rules, authController.register);
 router.post('/login', validateLogin.rules, authController.login);
 
 // router.use(checkAuth);
 
-router.put('/update-profile', checkAuth,uploadProfileImg, validateProfile.rules, authController.updateProfile);
-router.get('/get-profile', checkAuth,authController.getProfile);
+router.put('/update-profile', checkAuth, uploadProfileImg, validateProfile.rules, authController.updateProfile);
+router.get('/get-profile', checkAuth, authController.getProfile);
+
+
+// Passport.js Google auth routes
+router.get('/google', passport.authenticate('google', {session: false, scope: ['profile', 'email']}));
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: '/login',
+    scope: ['email', 'openid', 'profile'],
+    session: false
+}), (req, res) => {
+    // console.log('routes!!!!')
+    // console.log(req.user.toJSON())
+    let token = jwt.sign(req.user.toJSON(), 'secretkey', {expiresIn: '8h'});
+    res.redirect(`${process.env.FRONTEND_URL}/?token=${token}`);
+});
 
 
 module.exports = router;
