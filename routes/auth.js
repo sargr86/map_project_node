1995c80b98a3157d2dbbda5729d77559740000c6
Sofia Bruno <ivan.nikitich.90@inbox.ru>
2020-01-14 13:56:27 +0400

    Added facebook auth

--- routes/auth.js
+++ routes/auth.js
@@ -28,4 +28,15 @@ router.get('/google/callback', passport.authenticate('google', {
 });
 
 
+// Passport.js Facebook auth routes
+router.get('/facebook', passport.authenticate('facebook', {session: false}));
+router.get('/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/login',
+    scope: ['email', 'openid', 'profile'],
+    session: false
+}), (req, res) => {
+    let token = jwt.sign(req.user, 'secretkey', {expiresIn: '8h'});
+    res.redirect(`${process.env.FRONTEND_URL}/?token=${token}`);
+});
+
 module.exports = router;
