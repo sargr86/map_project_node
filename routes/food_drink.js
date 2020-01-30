de4242bb4359cdc655f207a0f215dcd0b569206c
Sofia Bruno <sofiabruno3003@gmail.com>
2020-01-30 17:27:36 +0400

    Fix for food-drink files variable, food-drink routes, change detection fix attempt for sidebar

--- routes/food_drink.js
+++ routes/food_drink.js
@@ -5,8 +5,8 @@ const validateFoodDrink = require('../validators/validateFoodDrink');
 router.get('/get', foodDrinkController.get);
 router.get('/get-partners', foodDrinkController.getPartners);
 router.get('/getOne', foodDrinkController.getOne);
-router.post('/add', uploadTourImg, validateFoodDrink.rules, foodDrinkController.add);
-router.put('/update', uploadTourImg, validateFoodDrink.rules, foodDrinkController.update);
+router.post('/add', uploadImages, validateFoodDrink.rules, foodDrinkController.add);
+router.put('/update', uploadImages, validateFoodDrink.rules, foodDrinkController.update);
 router.delete('/remove', foodDrinkController.remove);
 
 module.exports = router;
