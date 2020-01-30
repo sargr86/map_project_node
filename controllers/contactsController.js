93e0d1942411237a34419f8edf01ddb044ba60ed
Sofia Bruno <ivan.nikitich.90@inbox.ru>
2020-01-30 11:17:02 +0400

    Added contacts add/remove/view functionality

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,3 +2,20 @@ exports.request = async (req, res) => {
     await Contacts.create(req.body);
     res.json('OK');
 };
+
+
+exports.get = async (req, res) => {
+    const contacts = await Contacts.findAll();
+    res.json(contacts);
+};
+
+exports.getOne = async (req, res) => {
+    const contacts = await Contacts.findOne({where:{id:req.query.id},attributes: {exclude: ['id','accepted']}});
+    res.json(contacts);
+};
+
+exports.remove = async (req, res) => {
+    let data = req.query;
+    await Contacts.destroy({where: {id: data.id}});
+    this.get(req, res);
+};
--- migrations/20200127134315-create-contacts.js
+++ migrations/20200127134315-create-contacts.js
@@ -8,7 +8,7 @@ module.exports = {
                 primaryKey: true,
                 type: Sequelize.INTEGER
             },
-            company_title: {
+            company_name: {
                 type: Sequelize.STRING
             },
             email: {
