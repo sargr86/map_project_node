743d75a873c9f66d40ac94005f330d8cd4e41205
Sofia Bruno <sofiabruno3003@gmail.com>
2020-01-31 16:49:03 +0400

    Remove-image test

--- helpers/removeImage.js
+++ helpers/removeImage.js
@@ -1,7 +1,7 @@
 const rimrafPromise = require('rimraf-promise');
 
 module.exports = async (req, res) => {
-    let filePath = path.resolve(__dirname + '\\..\\public/' + req.filename.replace(/^.*\/\/[^\/]+/, ''));
+    let filePath = path.resolve(__dirname + '/../public/' + req.filename.replace(/^.*\/\/[^\/]+/, ''));
     console.log(filePath)
     if (fse.existsSync(filePath)) {
         await rimrafPromise(filePath);
