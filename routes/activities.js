95f6acf08ad0a747623e9335675328e4cbe0ab44
Sofia Bruno <sofiabruno3003@gmail.com>
2020-02-12 13:27:32 +0400

    Activities form fixes

--- routes/activities.js
+++ routes/activities.js
@@ -5,8 +5,9 @@ const validateActivity = require('../validators/validateActivity');
 router.get('/get', activitiesController.get);
 router.get('/get-partners', activitiesController.getPartners);
 router.get('/getOne', activitiesController.getOne);
-router.post('/add', uploadTourImg, validateActivity.rules, activitiesController.add);
-router.put('/update', uploadTourImg, validateActivity.rules, activitiesController.update);
+router.post('/add', uploadImages, validateActivity.rules, activitiesController.add);
+router.put('/update', uploadImages, validateActivity.rules, activitiesController.update);
+router.put('/make-cover', checkAuth, checkRole('admin', 'partner'), activitiesController.makeCover);
 router.delete('/remove', activitiesController.remove);
-
+router.delete('/remove-image', activitiesController.removeImage);
 module.exports = router;
