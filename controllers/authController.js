8f71c9fe5ba48d8fc10b696e4b749a098a113520
Sofia Bruno <finn@webmasterfreelanceonlinedev.site>
2020-03-09 13:38:22 +0400

    Added forgot-password

--- controllers/authController.js
+++ controllers/authController.js
@@ -285,7 +285,7 @@ exports.forgotPassword = async (req, res) => {
 
         // send mail with defined transport object
         transporter.sendMail(mailOptions, (error, info) => {
-            console.log('aaaaaa')
+            console.log(error)
             if (error) {
                 res.status(500).json({msg: error.toString()})
             } else if (info) {
