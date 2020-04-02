9686a7c571835a18697016d0f5a38e2db7974ce6
Sofia Bruno <sofiabruno3003@gmail.com>
2020-04-02 12:05:33 +0400

    Latitude/longitude decimal

--- models/ferries_directions.js
+++ models/ferries_directions.js
@@ -2,8 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const ferries_directions = sequelize.define('ferries_directions', {
     name: DataTypes.STRING,
-    latitude: DataTypes.STRING,
-    longitude: DataTypes.STRING
+    latitude: DataTypes.DECIMAL(10, 6),
+    longitude: DataTypes.DECIMAL(11, 6)
   }, {underscored: true});
   ferries_directions.associate = function(models) {
     // associations can be defined here
