7ccab935b85d57d1b383882d8d952f369449088e
Sofia Bruno <sofiabruno3003@gmail.com>
2020-04-16 10:42:07 +0400

    Updated socket.js

--- socket.js
+++ socket.js
@@ -17,6 +17,7 @@ exports.socket = (io) => {
         socket.on('newUser', (user) => {
             console.log(`${user.socket_nickname} is a new user`);
             users[user.socket_nickname] = socket;
+            socket.emit('newUser', 'connected')
             updateConnectedUsers(user);
         });
 
@@ -24,17 +25,20 @@ exports.socket = (io) => {
         // Send message
         socket.on('sendMessage', async (data) => {
             let receiver = data.to;
-            await chatController.create(data);
-            socket.emit('messageSent', data);
+            let currentMsg = await chatController.create(data);
+            console.log(currentMsg)
+            socket.emit('messageSent', currentMsg);
             console.log(Object.keys(users))
+
             if (users[receiver]) {
-                users[receiver].emit('messageSent', data);
+                console.log('Receiver found!!!')
+                users[receiver].emit('messageSent', currentMsg);
             } else console.log('Receiver not found!!!')
         });
 
         //Someone is typing a message
-        socket.on('typing',(data,callback)=>{
-            socket.broadcast.emit('typing',data)
+        socket.on('typing', (data, callback) => {
+            socket.broadcast.emit('typing', data)
         });
 
         // Create order by customer
@@ -82,9 +86,18 @@ exports.socket = (io) => {
             data.id = data._id;
             await getChangedOrderSendBack(data, 'finished', 'orderFinished');
         });
+        // Log out
+        socket.on('logout', () => {
+            console.log(socket.username + ' logout')
+            if (!socket.username) return;
+            delete users[socket.username];
+            connectedUsers = connectedUsers.filter(u => u.username !== socket.username);
+            io.sockets.emit('update-usernames', connectedUsers)
+        });
 
         // Disconnect
         socket.on('disconnect', () => {
+            console.log('disconnect')
             socketIDs.splice(socketIDs.indexOf(socket), 1);
             delete users[socket.username]; // removing by saved username in newUser event
             connectedUsers = connectedUsers.filter(u => u.username !== socket.username);
@@ -92,13 +105,17 @@ exports.socket = (io) => {
             console.log('user disconnected')
         });
 
-        function updateConnectedUsers(user) {
-            let username = user.socket_nickname;
-            socket.username = username; // for disconnect
-            if (!(connectedUsers.find(u => u.username === username))) {
-                connectedUsers.push({username, email: user.email, id: user.id});
+        function updateConnectedUsers(user = {}) {
+            if (Object.keys(user).length === 0 && user.constructor === Object) {
+                return '';
+            } else {
+                let username = user.socket_nickname;
+                socket.username = username; // for disconnect
+                if (!(connectedUsers.find(u => u.username === username))) {
+                    connectedUsers.push({username, email: user.email, id: user.id, socket_nickname: user.socket_nickname});
+                }
+                io.sockets.emit('update-usernames', connectedUsers)
             }
-            io.sockets.emit('update-usernames', connectedUsers)
         }
 
         async function getChangedOrderSendBack(data, status, eventBack) {
