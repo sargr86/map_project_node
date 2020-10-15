68747e01dbafa8472130b5244294d2244ab8452c
Sofia Bruno <sofiabruno3003@gmail.com>
2020-10-15 17:30:14 +0400

    Fixed activity types/subtypes seeders issue, used tour types data for tours home page

--- models/tours_type.js
+++ models/tours_type.js
@@ -1,11 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const tours_type = sequelize.define('tours_type', {
-    tour_name: DataTypes.STRING
-  }, {timestamps:false,tableName: 'tours_type'});
-  tours_type.associate = function(models) {
-    // associations can be defined here
-    tours_type.belongsTo(models.tours, {foreignKey: "id"});
-  };
-  return tours_type;
+    const tours_type = sequelize.define('tours_type', {
+        name: DataTypes.STRING,
+        description: DataTypes.TEXT
+
+    }, {timestamps: false, tableName: 'tours_type'});
+    tours_type.associate = function (models) {
+        // associations can be defined here
+        tours_type.belongsTo(models.tours, {foreignKey: "id"});
+    };
+    return tours_type;
 };
--- seeders/20190426122417-default_activity_types.js
+++ seeders/20190426122417-default_activity_types.js
@@ -3,6 +3,9 @@ require('../constants/main');
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         const data = await fse.readJSON(path.resolve(__dirname + '/data/activity_types.json'));
+        data.map(d => {
+            delete d.subtypes;
+        });
         return queryInterface.bulkInsert('activity_types', data)
     },
 
