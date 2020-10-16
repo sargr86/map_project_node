7c31ee639e241757aeeeec3e6e1057fcecb524d8
Sofia Bruno <sofiabruno3003@gmail.com>
2020-10-16 10:26:22 +0400

    Migration fix

--- seeders/20201001054832-default_activity_subtypes.js
+++ seeders/20201001054832-default_activity_subtypes.js
@@ -6,8 +6,9 @@ const path = require('path');
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         const data = await fse.readJSON(path.resolve(__dirname + '/data/activity_types.json'));
+        console.log(data)
         const list = data.map(async (type) => {
-            // console.log(type.name)
+            console.log(type.name)
             const type_id = await queryInterface.rawSelect('activity_types', {
                 where: {
                     name: type.name
@@ -17,6 +18,8 @@ module.exports = {
 
             return type.hasOwnProperty('subtypes') ? type.subtypes.map(t => {
                 t.type_id = type_id;
+                t.created_at = new Date.now();
+                console.log(t)
                 return t;
             }) : null;
         });
