de5f06fe7c019fb145b00ee2f8a88fa5f30b50be
Davit Burton <davitburton280@gmail.com>
2021-01-05 14:36:41 +0400

    Tours recent changes

--- routes/tour_types.js
+++ routes/tour_types.js
@@ -3,7 +3,7 @@ const toursController = require('../controllers/toursController');
 const validateTourType = require('../validators/validateTourType');
 
 router.get('/get', toursController.getTourTypes);
-router.get('/getOne', checkAuth, toursController.getOneTourType);
+router.get('/get-one', checkAuth, toursController.getOneTourType);
 router.post('/add', checkAuth, validateTourType.rules, toursController.addTourType);
 router.put('/update', checkAuth, validateTourType.rules, toursController.updateTourType);
 router.delete('/remove', checkAuth, toursController.removeTourType);
