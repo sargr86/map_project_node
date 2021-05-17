cd9314018000142d69e01751a32bb7c144e776b1
Margaret Vann <margaretvann69@yahoo.com>
2021-05-17 11:32:18 +0400

    Attempt to fix Token format for some cases

--- helpers/checkAuth.js
+++ helpers/checkAuth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+    let token = req.headers['authorization']; // Express headers are auto converted to lowercase
     // if(process.env.NODE_ENV === 'production')
     if (!token) {
         res.status(500).send('Auth token is not supplied');
@@ -10,18 +10,18 @@ module.exports = (req, res, next) => {
             console.log(token)
             token = token.slice(7, token.length);
         }
-
-            jwt.verify(token, 'secretkey', (err, decoded) => {
-                if (err) {
-                    console.log(err)
-                    return res.json({
-                        success: false,
-                        message: 'Token is not valid'
-                    });
-                } else {
-                    req.decoded = decoded;
-                    return next();
-                }
-            });
+        console.log(token)
+        jwt.verify(token, 'secretkey', (err, decoded) => {
+            if (err) {
+                console.log(err)
+                return res.json({
+                    success: false,
+                    message: 'Token is not valid'
+                });
+            } else {
+                req.decoded = decoded;
+                return next();
+            }
+        });
     }
 };
